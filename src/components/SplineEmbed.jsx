376c812d2310bf24b093d67f80825139b3fee76b
thebluedev <bluefurythebest@gmail.com>
2024-05-27 23:03:24 +0530

    changed 3D model to actual product

--- src/components/SplineEmbed.jsx
+++ src/components/SplineEmbed.jsx
@@ -2,6 +2,6 @@ import Spline from "@splinetool/react-spline";
 
 export default function SplineEmbed() {
   return (
-    <Spline scene="https://prod.spline.design/MEvPa2rlyNMn4Z1s/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/TterjLteT9kjB4iI/scene.splinecode" />
   );
 }
